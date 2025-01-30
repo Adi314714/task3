dc3688b6e56892edc7c71abc2871328c4744d6d1
Adi314714 <adiazar10@gmail.com>
2025-01-30 13:11:57 +0200

    ex1-finish (b)

--- part B/ex1.js
+++ part B/ex1.js
@@ -12,10 +12,15 @@ function PasswordInpKeyUP(element) {
   const lengthPass = passwordInp.value.length;
   console.log(lengthPass);
 
+  //(8) בדיקת אורך
+  let longerThan8 = false;
+  if (lengthPass >= 8) {
+    longerThan8 = true;
+  }
+
   // בדיקה אם יש מספר
   const arrPassword = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  let containsNumbers = false;
-  let containSpecial = false;
+  let containsNmubers = false;
   arrPassword.forEach((number) => {
     if (passwordInp.value.includes(number)) {
       let containsNumbers = true;
@@ -24,14 +29,54 @@ function PasswordInpKeyUP(element) {
 
   // בדיקה אם יש תווים מיוחדים
   const ArrSpecialChars = ["!", "@", "#", "$", "%", "*", "+", "-", "&", "|"];
+  let specialChars = false;
   ArrSpecialChars.forEach((number) => {
     if (passwordInp.value.includes(number)) {
-      let specialChars = true;
+      specialChars = true;
     }
   });
+  // בדיקה אם יש אותיות גדולות
+  let containsUppercase = false;
+  if (passwordInp.value !== passwordInp.value.toLowerCase()) {
+    containsUppercase = true;
+  }
+
+  console.log("containsNmubers:" + containsNmubers);
+  console.log("specialChars:" + specialChars);
+  console.log("containsUppercase:" + containsUppercase);
+  console.log("longerThan8:" + longerThan8);
+  console.log(
+    changeColor(
+      passworStength(
+        containsNmubers,
+        specialChars,
+        containsUppercase,
+        longerThan8
+      )
+    )
+  );
 }
-// בדיקה אם יש אותיות גדולות
-let containsUppercase = false;
-if (passwordInp.value === passwordInp.value.tolowercase()) {
-  containsUppercase = true;
+
+function passworStength(
+  containsNmubers,
+  specialChars,
+  containsUppercase,
+  longerThan8
+) {
+  if (containsNmubers && specialChars && containsUppercase && longerThan8) {
+    return "green";
+  } else if (
+    !containsNmubers &&
+    !specialChars &&
+    !containsUppercase &&
+    !longerThan8
+  ) {
+    return "red";
+  } else {
+    return "yellow";
+  }
+}
+
+function changeColor(color) {
+  indicatorDiv.style.backgroundColor = color;
 }
